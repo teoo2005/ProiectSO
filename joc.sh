cfdc82aa80691825649b0d2c99b4c1dd2fc30dbf
teoo2005 <94323707+teoo2005@users.noreply.github.com>
2025-05-28 20:39:37 +0300

    Update joc.sh

--- joc.sh
+++ joc.sh
@@ -4,22 +4,33 @@ echo "Joc: piatra, foarfeca, hartie"
 echo "Alege: piatra, foarfeca sau hartie"
 read alegere
 
+# vector de optiuni
 PFH=("piatra" "foarfeca" "hartie")
+
+# joc- adica ce alege calculatorul cu functia random
 joc=${PFH[$RANDOM % 3]}
 
 echo "Calculatorul a ales: $joc"
 
+# facem o serie de if uri sa abordam taote cazurile
 if [ "$alegere" == "$joc" ]; then
     echo "EGALTATE!"
-    echo ""
-elif [[ ( "$alegere" == "piatra" && "$joc" == "foarfeca" ) || \
-        ( "$alegere" == "foarfeca" && "$joc" == "hartie" ) || \
-        ( "$alegere" == "hartie" && "$joc" == "piatra" ) ]]; then
-    echo "BRAVO MA ! Ai castigat!"
-    echo ""
-elif [[ ( "$alegere" == "piatra" && "$joc" == "hartie" ) || \
-        ( "$alegere" == "foarfeca" && "$joc" == "piatra") || \
-        ( "$alegere" == "hartie" && "$joc" == "foarfeca") ]]; then
-    echo "Calculatorul a castigat!"
-    echo ""
+elif [ "$alegere" == "piatra" ]; then
+    if [ "$joc" == "foarfeca" ]; then
+        echo "BRAVO MA ! Ai castigat!"
+    else
+        echo "Calculatorul a castigat!"
+    fi
+elif [ "$alegere" == "foarfeca" ]; then
+    if [ "$joc" == "hartie" ]; then
+        echo "BRAVO MA ! Ai castigat!"
+    else
+        echo "Calculatorul a castigat!"
+    fi
+elif [ "$alegere" == "hartie" ]; then
+    if [ "$joc" == "piatra" ]; then
+        echo "BRAVO MA ! Ai castigat!"
+    else
+        echo "Calculatorul a castigat!"
+    fi
 fi
