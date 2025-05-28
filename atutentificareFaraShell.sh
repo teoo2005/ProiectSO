8d903f9252a8e51f45cbe3ff8cbb61a14e8dd3d8
teoo2005 <94323707+teoo2005@users.noreply.github.com>
2025-05-28 21:53:41 +0300

    Update atutentificareFaraShell.sh

--- atutentificareFaraShell.sh
+++ atutentificareFaraShell.sh
@@ -4,69 +4,72 @@ while true; do
     echo "Introduceti numele de utilizator pentru autentificare:"
     read nume
 
-    # salvam intrun contor sa vedem daca se gaseste numele sau nu 
+    # salvam intrun contor sa vedem daca se gaseste numele sau nu
     contor=$(grep ",$nume," utilizatori.csv)
 
-    # daca contorul e gol , adica nu s-a gasit numele , te duce in main 
+    # daca contorul e gol , adica nu s a gasit numele , te duce in main
     if [ -z "$contor" ]; then
         echo "Eroare: Utilizatorul $nume nu exista inregistrat. Va rugam sa va inregistrati mai intai."
         echo "Mergeti in meniul principal la inregistrare."
-        break  # break folosim ca sa iasa din script si sa te poti duce inapoi in main 
+     break  #break folosim ca sa iasa din script si sa te poti duce inapoi in main
     fi
 
-    # extragem parola stocata deja , cea cu hash 
+    # extragem parola stocata deja , cea cu hash
     parolaStocata=$(echo "$contor" | sed 's/^[^,]*,[^,]*,[^,]*,\([^,]*\).*$/\1/')
    # ^[^,]*, — de la începutul liniei (^), selectează orice caractere care nu sunt virgule ([^,]*) urmate de o virgulă (de 3 ori).
    #\([^,]*\) — apoi captează (\(...\)) tot ce nu e virgulă, adică al patrulea câmp, care este parola criptată (hash-ul).
    #.*$ — restul liniei (oricare caractere până la sfârșit).
-   
-    
+
+
     # parola autentificare
     echo "Introduceti parola:"
     read -s parolaTastata
 
-    # criptare parola 
-    parolaTastataHash=$(echo -n "$parolaTastata" | sha256sum | sed 's/\s.*//') # varianta mai simpla 
-    
+    # criptare parola
+    # parolaTastataHash=$(echo -n "$parolaTastata" | sha256sum | sed 's/^\([a-f0-9]\{64\}\)\s.*$/\1/')
+
+    parolaTastataHash=$(echo -n "$parolaTastata" | sha256sum | sed 's/\s.*//') # varianta mai simpla
+
     # Verificam daca hash-ul parolei este corect
     if [ "$parolaTastataHash" != "$parolaStocata" ]; then
-        echo "Eroare: Parola introdusa nu este corecta. Trebuie sa fie exact parola introdusa la inregistrare."
+        echo "Eroare: Parola introdusa nu este corecta. Trebuie sa fie exact parola inttrodusa la inregistrare ."
         continue
     fi
 
-    # Daca parola este corecta, actualizam campul de last login 
+    # Daca parola este corecta, actualizam campul de last login
+
     timp=$(date '+%Y-%m-%d %H:%M:%S')
     sed -i "s/^\([^,]*,$nume,[^,]*,[^,]*\).*/\1,$timp/" utilizatori.csv
 
     # extragere id-ul utilizatorului
     id=$(echo "$contor" | cut -d',' -f1)
-    
-    # Verificare daca utilizatorul exista deja in vectorul utlog
-    if [[ " ${utlog[@]} " =~ " $nume " ]]; then
-        echo "Utilizatorul $nume este deja logat!"
-    else
-        # Daca utilizatorul nu este deja in lista, adaugam in vectorul utlog
-        utlog+=("$nume")
-        echo "Utilizatorul $nume este acum autentificat si logat."
-    fi
-    
-    echo "Autentificare reusita! Esti acum in directorul tau personal: /home/$id"
-    cd "/home/$id" || exit 1 # daca nu merge, te scoate in main , inseamna ca a aparut o eroare
-    $SHELL # inlocuieste procesul curent shell cu un nou shell în directorul utilizatorului
 
-    # Loop-ul pentru activitatea utilizatorului in directorul sau personal
+#Verificare daca exista utilizator logat deja
+if [[ " ${utlog[@]} " =~ " $nume " ]]; then
+    echo "Utilizatorul $nume exista deja in utlog "
+else
+    utlog+=("$nume")
+    echo "Utilizatorul $nume este acum logat in utlog ."
+fi
+
+    echo "Acum esti autenfificat."
+
+        echo "Autentificare reusita! Esti acum in directorul tau personal: /home/$id"
+       cd "/home/$id" || exit 1 # daca nu merge, te scoate in main , inseamna ca o dat eroare
+       $SHELL # inlocuieste procesul curent shell cu un nou shell în directorul utilizatorului
+
+       # Loop-ul pentru activitatea utilizatorului in directorul sau personal
     while true; do
-        echo -n "Daca vrei sa iesi din directorul personal, scrie 'exit'. Pentru a continua, apasa orice tasta si apasa ENTER: "
+        echo -n "Daca vrei sa iesi din directorul personal, scrie 'iesire'. Pentru a continua, apasa orice tasta si apasa ENTER: "
         read comanda
 
-        if [ "$comanda" == "exit" ]; then
-            echo "Iesire din directorul personal..."
-            cd /mnt/c/Users/teodo #aici am eu , voi va puneti unde aveti voi salvat scriptul
+        if [ "$comanda" == "iesire" ]; then
+            cd /mnt/c/Users/teodo
             break  # iei utilizatorul inapoi in meniul principal
         else
             echo "Continua sa lucrezi in directorul tau personal..."
         fi
     done
-    
+
     break  # Iesi din bucla principala si revii in main.sh
 done
