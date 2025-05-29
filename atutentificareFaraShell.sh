909f7cbcdf44d1cbaf316953c1f7d1e076f79790
teoo2005 <94323707+teoo2005@users.noreply.github.com>
2025-05-29 20:20:13 +0300

    Update atutentificareFaraShell.sh

--- atutentificareFaraShell.sh
+++ atutentificareFaraShell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    echo "Introduceti numele de utilizator pentru autentificare:"
+    echo "Scrie numele de utilizator pentru autentificare:"
     read nume
 
     # salvam intrun contor sa vedem daca se gaseste numele sau nu
@@ -9,8 +9,8 @@ while true; do
 
     # daca contorul e gol , adica nu s a gasit numele , te duce in main
     if [ -z "$contor" ]; then
-        echo "Eroare: Utilizatorul $nume nu exista inregistrat. Va rugam sa va inregistrati mai intai."
-        echo "Mergeti in meniul principal la inregistrare."
+        echo "EROARE! Utilizatorul $nume nu exista. Inregistreaza te mai intai."
+        echo "Mergi in meniu la sectia de inregistrare."
      break  #break folosim ca sa iasa din script si sa te poti duce inapoi in main
     fi
 
@@ -22,29 +22,26 @@ while true; do
 
 
     # parola autentificare
-    echo "Introduceti parola:"
+    echo "Scrie parola:"
     read -s parolaTastata
 
     # criptare parola
-    # parolaTastataHash=$(echo -n "$parolaTastata" | sha256sum | sed 's/^\([a-f0-9]\{64\}\)\s.*$/\1/')
-
     parolaTastataHash=$(echo -n "$parolaTastata" | sha256sum | sed 's/\s.*//') # varianta mai simpla
 
-    # Verificam daca hash-ul parolei este corect
+    # verificare daca hash ul parolei este corect
     if [ "$parolaTastataHash" != "$parolaStocata" ]; then
-        echo "Eroare: Parola introdusa nu este corecta. Trebuie sa fie exact parola inttrodusa la inregistrare ."
+        echo "EROARE! Parola nu este corecta. Trebuie sa fie exact parola inttrodusa la inregistrare ."
         continue
     fi
 
-    # Daca parola este corecta, actualizam campul de last login
-
+    # daca parola este corecta, actualizam campul de last login
     timp=$(date '+%Y-%m-%d %H:%M:%S')
     sed -i "s/^\([^,]*,$nume,[^,]*,[^,]*\).*/\1,$timp/" utilizatori.csv
 
     # extragere id-ul utilizatorului
    id=$(echo "$contor" | sed  's/^\([^,]*\),.*$/\1/') #id=$(echo "$contor" | sed 's/,.*//')
 
-#Verificare daca exista utilizator logat deja
+#verificare daca exista utilizator logat deja 
 if [[ " ${utlog[@]} " =~ " $nume " ]]; then
     echo "Utilizatorul $nume exista deja in utlog "
 else
@@ -52,15 +49,15 @@ else
     echo "Utilizatorul $nume este acum logat in utlog ."
 fi
 
-    echo "Acum esti autenfificat."
+    echo "Acum esti autenfificat"
 
-        echo "Autentificare reusita! Esti acum in directorul tau personal: /home/$id"
-       cd "/home/$id"    # || exit 1  daca nu merge, te scoate in main , inseamna ca o dat eroare
+        echo "Autentificare reusita! Esti acum in directorul tau : /home/$id"
+       cd "/home/$id"   
        $SHELL # inlocuieste procesul curent shell cu un nou shell în directorul utilizatorului
 
-       # Loop-ul pentru activitatea utilizatorului in directorul sau personal
+       # while folosit ca sa poti iesi din directorul tau
     while true; do
-        echo -n "Daca vrei sa iesi din directorul personal, scrie 'iesire'. Pentru a continua, apasa orice tasta si apasa ENTER: "
+        echo -n "Daca vrei sa iesi din director, scrie 'iesire'. Pentru a continua, apasa orice tasta si apasa ENTER: "
         read comanda
 
         if [ "$comanda" == "iesire" ]; then
@@ -68,7 +65,7 @@ fi
                                   #pt putty  cd /home/stud1018
             break  # iei utilizatorul inapoi in meniul principal
         else
-            echo "Continua sa lucrezi in directorul tau personal..."
+            echo "Continua sa lucrezi in directorul tau personal"
         fi
     done
 
