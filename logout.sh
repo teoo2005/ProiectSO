1d425903cf63a3c6e9e8f2d1b79863b795504be8
teoo2005 <94323707+teoo2005@users.noreply.github.com>
2025-05-29 20:13:41 +0300

    Update logout.sh

--- logout.sh
+++ logout.sh
@@ -2,12 +2,11 @@
 # aici nu stiu cat de eficient este , fiindca cand se sterge utiliz.
 # ar putea ramane spatiu gol in vector;
 
-echo "Introduceti numele de utilizator pentru logout:"
+echo "numele de utilizator pentru logout:"
 read nume
 
-# Verificam daca utilizatorul este autentificat in lista utlog
+# verificam daca utiliz este autentificat in lista utlog
 if [[ " ${utlog[@]} " =~ " $nume " ]]; then
-    # Eliminam utilizatorul din lista de autentificati
     utlog=("${utlog[@]/$nume}")
     echo "Utilizatorul $nume a fost deconectat."
 else
